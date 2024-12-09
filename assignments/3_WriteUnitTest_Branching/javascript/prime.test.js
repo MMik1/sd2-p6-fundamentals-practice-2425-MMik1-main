190ba89c06c227db29ad022012045575779bd6a4
MMik1 <m.mik@sintlucasedu.nl>
2024-12-09 14:15:15 +0100

    retfddgf

--- assignments/3_WriteUnitTest_Branching/javascript/prime.test.js
+++ assignments/3_WriteUnitTest_Branching/javascript/prime.test.js
@@ -3,7 +3,13 @@ import {isPrime, getPrimesUpTo} from "./prime.js";
 
 
 // 2. Write a unit test for the isPrime function (use Vitest: https://vitest.dev/guide/) to test if 2 is a prime number.
+export function sum(a, b) {
+    return a + b
+}
 
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3)
+})
 
 // 2. Write a unit test for the isPrime function (use Vitest: https://vitest.dev/guide/) to test if 4 is a prime number.
 
